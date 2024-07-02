60f661e212a9e2a864ab4fcafea89270fafef491
dennisppaul <dennis.paul@hfk-bremen.de>
2024-07-02 18:18:06 +0200

    minor changes

--- src/klangstrom-emulator-standalone.cpp
+++ src/klangstrom-emulator-standalone.cpp
@@ -1,3 +1,6 @@
+#define KLANG_SAMPLES_PER_AUDIO_BLOCK 2048
+#define KLANG_SAMPLING_RATE           48000
+
 #include "Arduino.h"
 #include "Klangstrom.h"
 #include "KlangstromAudioCodec.h"
@@ -5,18 +8,13 @@
 #include "KlangstromLEDs.h"
 #include "Wavetable.h"
 
-#define KLANG_SAMPLES_PER_AUDIO_BLOCK DEFAULT_FRAMES_PER_BUFFER
-#define KLANG_SAMPLING_RATE DEFAULT_AUDIO_SAMPLE_RATE
-
-void audioblock(float** input_signal, float** output_signal, uint16_t length);
-
 SerialDebug console;
 Klangstrom  klangstrom;
 AudioCodec  audiocodec;
 LEDs        leds;
 uint8_t     fLEDIntensity = 0;
 
-float     wavetable[512];
+float                  wavetable[512];
 klangwellen::Wavetable oscillator{wavetable, 512, 48000};
 
 void setup() {
@@ -48,17 +46,18 @@ void setup() {
 
 void loop() {
     fLEDIntensity += 10;
-    leds.set(0, fLEDIntensity / 255.0);
+    leds.set(0, (float) fLEDIntensity / 255.0f);
     leds.toggle(1);
     // console.println("LED 0 : %f", leds.get(0));
     // console.println("LED 1 : %f", leds.get(1));
-    oscillator.set_frequency(random(1, 4) * 55);
+    oscillator.set_frequency((float) random(1, 4) * 55);
 
     klangstrom.loop();
     delay(1000);
 }
 
 void audioblock(float** input_signal, float** output_signal, uint16_t length) {
+    (void) input_signal;
     for (int i = 0; i < length; ++i) {
         output_signal[0][i] = oscillator.process();
         output_signal[1][i] = output_signal[0][i];
